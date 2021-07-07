28d7616dc292ded21b4713d53e1b61a2cfada28b
zague77 <alex.j.alves@gmail.com>
2021-07-07 11:04:24 +0100

    xhr avec bind

--- js/xhr.js
+++ js/xhr.js
@@ -1,7 +1,48 @@
-function XHR(baseUrl){
+function XHR(baseUrl) {
+    var _baseUrl = baseUrl;
+    var _xhr;
+    var _xhrthis=this;
+    this.lastResult = undefined;
+
+    function _request(method, ressourceUrl, callback, bodyStr) {
+            //1.instanciation
+            _xhr=new XMLHttpRequest();
+            //2. open
+            _xhr.open(method,baseUrl+ressourceUrl);
+            //2.b header
+        _xhr.setRequestHeader('Content-type', 'application/json');
+            //3 gestionnaire evt
+
+            //window
+            console.log(this);
+
+            _xhr.onreadystatechange=(function(evt){
+                if(_xhr.readyState<XMLHttpRequest.DONE) return;
+                if(_xhr.status!==200 && _xhr.status!=201){
+                    console.log('ERROR');
+                }
+                console.log(this);
+                _xhrthis.lastResult=_xhr.response;
+                if(undefined!==callback) callback(_xhr.response)
+                    
+                
+
+            }).bind(_xhrthis);
+
+            // bind sur la function
+
+            //4. send
+            _xhr.send(bodyStr);
+    }
+
+    this.get = function (ressourceUrl,callback) {
+        _request('GET',ressourceUrl,callback)
+    }
+
+    this.put = function (ressourceUrl,objectBody,callback) {
+        _request('PUT',ressourceUrl,callback,JSON.stringify(objectBody))
+    }
 
 
-function request(method,ressourceUrl,callback, bodyStr)
 
-    
 }
