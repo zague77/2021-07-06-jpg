545781b830abd04c371c8f2f955fc11052131a03
zague77 <alex.j.alves@gmail.com>
2021-07-07 14:07:50 +0100

    ES6

--- js/metier.js
+++ js/metier.js
@@ -1,81 +1,106 @@
-
-//es6
-//es6 , on met plus ";"
-/**
- * 
- */
+const REST_BASE_URL = 'localhost:5679'
 class Product {
-    price;
-    #name;
-    #id;
-
-    /***
-     * @param {number} id value of Id
+    static TVA = { ALIM: 5.5, NON_ALIM: 20.6 }
+    static RESSOURCE_NAME = '/products'
+    price
+    #name
+    #id
+    tva
+    /**
+     * cosntructor of product
+     * @param {number} id value  of id
      * @param {string} name value of name
      * @param {number} price value of price
      */
     constructor(id, name, price) {
-        this.price = price;
-        this.#name = name;
         this.#id = id;
+        //if (undefined === name && undefined === price) this.#_loading();
+        //else {
+        this.price = price
+        this.#name = name;
+        this.tva = Product.TVA.ALIM;
+        //}
     }
-
     /**
-     * return Id
-     * @returns (string) value of name
+     * rest load with an id
      */
-    get id(){
+    #_loading() {
+        const ressourceUrl = `http://${REST_BASE_URL}${Product.RESSOURCE_NAME}/${this.#id}`
+        const promise = fetch(ressourceUrl);
+        promise.then(
+            (response) => { return response.json() },
+            (unsuccessResponse) => { console.error('Une errur de chargement') }
+        ).then((obj) => {
+            console.log(obj);
+            this.loadValues(this.#id,obj.name,obj.price);
+            return obj;
+        })
+      
+    }
+    /**
+     * return id
+     * @returns {number} value of id
+     */
+    get id() {
         return this.#id;
     }
-
-    get name(){
+    /**
+     * return name
+     * @returns {string} value of name
+     */
+    get name() {
         return this.#name;
     }
-
-    set id(value){
-        this.#id = value;
-    }
-
-    set name(value){
-         this.#name = value;
-    }
-
-
-    //private function
     /**
-     * make a discount d'une fonction
+     * set name
+     * @param {string} value new value of name
+     */
+    set name(value) {
+        this.#name = value;
+    }
+    //private functions
+    /**
+     * make a discount on product price
      * @param {number} pcent 
      */
-    #_makeDiscount(pcent){
+    #_makeDiscount(pcent) {
         this.price = (this.price - (this.price * (pcent / 100))).toFixed(2);
     }
-
- 
-
-     tojsonString(){
-        return JSON.stringify(
-                Object.assign({name:this.#name, id:this.#id},this));
+    tojsonString() {
+        return JSON.stringify(Object.assign({ name: this.#name, id: this.#id }, this));
     }
-
-    makediscount=this.#_makeDiscount
-
- 
-
+    makediscount = this.#_makeDiscount
+    loadBasicValues=(id,name,price)=>{
+        this.#name = name;
+        this.price = price;
+        this.#id=id;
+    }
 }
 
-
-class Boat extends Product{
-    #_dimensions=[]
-    constructor(id,name,price){
-        super(id,name,price);
+class Boat extends Product {
+    #_dimensions = []
+    constructor(id, name, price) {
+        super(id, name, price);
+        this.#_loading();
     }
-
-    addDimension=(dimension)=>{
+    addDimension = (dimension) => {
         this.#_dimensions.push(dimension);
     }
-
-        get dimension(){
-            return this.#_dimensions;
-        }
+    get dimensions() {
+        return this.#_dimensions;
+    }
+    #_loading() {
+        const ressourceUrl = `http://${REST_BASE_URL}${Product.RESSOURCE_NAME}/${this.id}`
+        const promise = fetch(ressourceUrl);
+        promise.then(
+            (response) => { return response.json() },
+            (unsuccessResponse) => { console.error('Une errur de chargement',unsuccessResponse) }
+        ).then((obj) => {
+            console.log(obj);
+            
+            this.loadBasicValues(this.id,obj.name,obj.price);
+            this.#_dimensions=obj.dimensions
+            return obj;
+        })
+    }
 }
-
